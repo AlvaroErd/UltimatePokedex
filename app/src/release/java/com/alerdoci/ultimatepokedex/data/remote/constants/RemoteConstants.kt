41e7877f27f31ea5b8e5c7e510204c31c0b26329
AlvaroErd <108676373+AlvaroErd@users.noreply.github.com>
2023-05-23 12:26:56 +0200

    Delete RemoteConstants for release

--- app/src/release/java/com/alerdoci/ultimatepokedex/data/remote/constants/RemoteConstants.kt
+++
@@ -1,8 +0,0 @@
-package com.alerdoci.ultimatepokedex.data.datasources.remote
-
-class RemoteConstants {
-    companion object {
-        // Base Url for release
-        const val BASE_URL = "https://pokeapi.co/api/v2/"
-    }
-}
